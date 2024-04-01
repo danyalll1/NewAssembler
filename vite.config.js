d598c92d60840ba82e3b6403d2f72cd750058961
danyalll <ne.danyalll@gmail.com>
2024-04-01 20:15:18 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
+import { fileURLToPath, URL } from 'url';
 import vituum from 'vituum'
 import pug from '@vituum/vite-plugin-pug'
 import pages from 'vituum/plugins/pages.js'
 import imports from "vituum/plugins/imports.js";
 
 export default {
+    base: './',
     plugins: [
         vituum(),
-        pug({root: './resources'}),
+        pug({root: '/resources'}),
         imports({
-            paths: ['./resources/styles/*/**', './resources/scripts/*/**', './resources/assets/*/**']
+            paths: ['/resources/styles/*/**', '/resources/scripts/*/**', '/resources/assets/*/**']
         }),
-        pages({dir: './resources/templates/pages', root: './resources'},),
+        pages({
+            dir: './resources/templates/pages',
+            root: './resources',
+            normalizeBasePath: true
+        },),
     ],
     build: {
         minify: true,
@@ -24,8 +30,15 @@ export default {
             output: {
                 chunkFileNames: 'resources/scripts/[name].js',
                 entryFileNames: 'resources/scripts/[name].js',
+
             },
 
         }
     },
+    resolve: {
+        alias: [
+            {find: '@', replacement: fileURLToPath(new URL('/resources', import.meta.url))},
+        ]
+    }
+
 }
